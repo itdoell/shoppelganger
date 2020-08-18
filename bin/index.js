6f9a9a5bda4030a10d21e76407e4aea0b905d8e2
Ian Doell <itdoell@gmail.com>
2020-08-18 06:46:05 -0700

    Switches GraphQL queries to bulk operations

--- bin/index.js
+++ bin/index.js
@@ -7,14 +7,15 @@ const fetch = require("node-fetch");
 const yaml = require('js-yaml');
 const dir = require('os').homedir() + '/.shoppelganger'; //directory where we're saving the theme
 
-
 // setup arguments
 const options = yargs
- .usage('Usage: -c <env> -t <env>')
- .demandOption(['c','t'])
- .argv;
+    .usage('Usage: -c <env> -t <env>')
+    .demandOption(['c', 't'])
+    .argv;
 
-const config = yaml.load(fs.readFileSync('./config.yml', {encoding: 'utf-8'}));
+const config = yaml.load(fs.readFileSync('./config.yml', {
+    encoding: 'utf-8'
+}));
 
 // get necessary info from stores
 const copyStore = {
@@ -30,67 +31,177 @@ const toStore = {
 console.log(`Copying ${options.c} to ${options.t}!`);
 
 
-
 // Get products from store using GraphQL
 fetch(`https://${copyStore.store}/admin/api/graphql.json`, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "X-Shopify-Access-Token": copyStore.token
-  },
-  body: JSON.stringify({
-    query: `{
-      products(first: 5) {
-        edges {
-          cursor
-          node {
-            id
-            title
-            vendor
-            productType
-            images(first: 10) {
-              edges {
-                node {
-                  src
-                }
-              }
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Shopify-Access-Token": copyStore.token
+        },
+        body: JSON.stringify({
+            query: `mutation {
+        	bulkOperationRunQuery(
+        		query: """
+        		 {
+        			products {
+        		          edges {
+        		              cursor
+        		              node {
+        		                id
+        		                title
+        		                vendor
+        		                productType
+        		                images(first: 10) {
+        		                  edges {
+        		                    node {
+        		                      src
+        		                    }
+        		                  }
+        		                }
+        		                options {
+        		                  name
+        		                  values
+        		                }
+        		              metafields {
+        		                edges {
+        		                  node {
+        		                    id
+        		                  }
+        		                }
+        		              }
+        		              variants {
+        		                edges {
+        		                  node {
+        		                    id
+        		                    barcode
+        		                    compareAtPrice
+        		                    fulfillmentService {
+        		                      id
+        		                      inventoryManagement
+        		                    }
+        		                    weight
+        		                    weightUnit
+        		                    inventoryItem {
+        		                      id
+        		                      unitCost {
+        		                        amount
+        		                        currencyCode
+        		                      }
+        		                      countryCodeOfOrigin
+        		                      harmonizedSystemCode
+        		                      provinceCodeOfOrigin
+        		                      tracked
+        		                    }
+        		                    inventoryPolicy
+        		                    inventoryQuantity
+        		                    metafields {
+        		                      edges {
+        		                        node {
+        		                          id
+        		                        }
+        		                      }
+        		                    }
+        		                    price
+        		                    sku
+        		                    taxable
+        		                    taxCode
+        		                  }
+        		                }
+        		              }
+        		              vendor
+        		              totalInventory
+        		            }
+        		          }
+
+        		        }
+            		}
+            		"""
+            	) {
+            		bulkOperation {
+            			id
+            			status
+            		}
+            		userErrors {
+            			field
+            			message
+            		}
+            	}
+            }`
+        })
+    })
+    .then(result => {
+        console.log('received result', result);
+        return result.json();
+    })
+    .then(bulkOp => {
+        const bulkOperationGid = bulkOp.data.bulkOperationRunQuery.bulkOperation.id;
+        console.log(bulkOperationGid);
+        console.log(JSON.stringify(bulkOp));
+        setTimeout(function () {
+            pollBulkOp(bulkOperationGid);
+        }, 5000);
+
+    }).catch(err => console.error(err));
+
+
+function pollBulkOp(bulkOperationGid) {
+    console.log('called pollBulkOp')
+  fetch(`https://${copyStore.store}/admin/api/graphql.json`, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "X-Shopify-Access-Token": copyStore.token
+      },
+      body: JSON.stringify({
+          query: `{
+          node(id: "${bulkOperationGid}") {
+            ... on BulkOperation {
+              id
+              status
+              errorCode
+              createdAt
+              completedAt
+              objectCount
+              fileSize
+              url
+              partialDataUrl
             }
-            options {
-              name
-              values
-            }
-            variants(first: 100) {
-              edges {
-                node {
-                  id
-                  title
-                  position
-                  sku
-                  price
-                  images {
-                    src
-                  }
-                }
-              }
-            }
-            tags
           }
-        }
+        }`
+      })
+  }).then(result => {
+      return result.json();
+  }).then(body => {
+      console.log('this is the body');
+      console.log(JSON.stringify(body));
+
+      if(body.data.node.status !== 'COMPLETED') {
+          console.log('not done yet, try again')
+          setTimeout(() => {
+              pollBulkOp(bulkOperationGid)
+          }, 2000);
+      } else {
+          console.log('done, parse options');
+          parseBulkOp(body.data.node.url);
       }
-    }`
-  })
-})
-  .then(result => {
-    return result.json();
-  })
-  .then(products => {
 
-    buildProducts(products);
-}).catch(err => {
-    console.log(err);
-});
+  }).catch(err => console.error(err));
+
+}
 
 
+
+
+function parseBulkOp(url) {
+    console.log('fetch jsonl from url',url);
+    fetch(url)
+    .then(res => {
+        return res.text();
+    })
+    .then(out => {
+        console.log('Output: ', out);
+    }).catch(err => console.error(err));
+}
 
 
 
@@ -99,13 +210,13 @@ fetch(`https://${copyStore.store}/admin/api/graphql.json`, {
 function buildProducts(products) {
 
 
-    var productString = ``;
+    var productString = '';
 
     for (let i = 0; i < products.data.products.edges.length; i++) {
         let product = products.data.products.edges[i].node;
 
 
-        let variantString = ``;
+        let variantString = '';
         for (let i = 0; i < product.variants.edges.length; i++) {
             let variant = product.variants.edges[i].node;
 
@@ -121,13 +232,13 @@ function buildProducts(products) {
             `
             // console.log(variantString);
         }
-        var images = product.images.edges.map(x=> {
+        var images = product.images.edges.map(x => {
             return x.node;
         });
         var img = JSON.stringify(images[0])
         // console.log()
         var imageObj = JSON.parse(img);
-        console.log('IMAGE'+imageObj.src);
+        console.log('IMAGE' + imageObj.src);
         // console.log('image source',);
         productString += `Product${i}: productCreate(input: {
             title: "${product.title}",
@@ -150,25 +261,25 @@ function buildProducts(products) {
 
     // console.log(productString);
     fetch(`https://${toStore.store}/admin/api/graphql.json`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Shopify-Access-Token": toStore.token
-      },
-      body: JSON.stringify({
-        query: `mutation {
-          ${productString}
-        }`
-      })
-    })
-      .then(result => {
-        return result.json();
-      })
-      .then(data => {
-        // console.log("data returned:\n", data);
-        // console.log(JSON.stringify(data));
-        console.log('Finished creating products!')
-      });
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Shopify-Access-Token": toStore.token
+            },
+            body: JSON.stringify({
+                    query: `mutation {
+              ${productString}
+            }`
+            })
+        })
+        .then(result => {
+            return result.json();
+        })
+        .then(data => {
+            // console.log("data returned:\n", data);
+            // console.log(JSON.stringify(data));
+            console.log('Finished creating products!')
+        });
 }
 
 // try {
